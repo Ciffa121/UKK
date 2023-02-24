ad80fa85960001cd60df134253b76a967d9aefa3
cippa <115058290+Ciffa121@users.noreply.github.com>
2023-02-24 07:39:24 +0700

    Add files via upload

---
+++ halamanadmin.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+<style>
+    
+</style>
+    <h1><div class="container-fluid">
+    <div class="row flex-nowrap">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-black">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <span class="fs-5 d-none d-sm-inline">MASYARAKAT</span>
+                </a>
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link align-middle px-0"><ion-icon name="home-outline"></ion-icon>
+                            <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                        </a>
+                    </li>
+                    <li>
+
+                            <li class="w-100">
+                                <a href="datalaporan" class="nav-link px-0"> <span class="d-none d-sm-inline"><ion-icon name="swap-horizontal-outline"></ion-icon>Laporan Masuk</span>  </a>
+                            </li>
+                            <li>
+                                <a href="datalaporan" class="nav-link px-0"> <span class="d-none d-sm-inline"><ion-icon name="pulse-outline"></ion-icon>Generate Laporan</span> </a>
+                            </li>
+                        </ul>
+                        <li>
+                                <a href="Logout" class="nav-link px-0"> <span class="d-none d-sm-inline"><ion-icon name="pulse-outline"></ion-icon>LOGOUT</span> </a>
+                            </li>
+                     </li>
+                    <li>
+                 </ul>
+                 
+                <hr>
+                 </div>
+        </div>
+        <div class= text-center>SELAMAT DATANG DI APLIKASI PENGADUAN MASYARAKAT</div></div>
+</div> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
